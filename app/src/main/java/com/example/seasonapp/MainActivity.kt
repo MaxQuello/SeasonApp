8f2914c3e438ba5e5eaf84da140e52cdf49c11a5
gscaglione <gioachino.scaglione@community.unipa.it>
2023-06-08 20:03:41 +0200

    Versione 08/06 ore 18:34 porcodiddio
    problema tasto fluttuante che diocane apre il fragment prenota e dalla seconda in poi non lo carica più.

--- app/src/main/java/com/example/seasonapp/MainActivity.kt
+++ app/src/main/java/com/example/seasonapp/MainActivity.kt
@@ -32,28 +32,24 @@ open class MainActivity : AppCompatActivity() {
             true
         }
 
-        //   replaceFragment(HomeFragment())
 
         binding.bottomNavigationView.setOnItemSelectedListener { item ->
             when (item.itemId) {
-                R.id.homeFragment -> replaceFragment(HomeFragment())
-                R.id.notifyFragment -> replaceFragment(NotifyFragment())
-                R.id.contattiFragment -> replaceFragment(NotifyFragment())
-                R.id.profileFragment -> replaceFragment(ProfileFragment())
+                R.id.homeFragment -> HomeFragment()
+                R.id.notifyFragment -> NotifyFragment()
+                R.id.contattiFragment -> NotifyFragment()
+                R.id.profileFragment -> ProfileFragment()
             }
             true
         }
-        binding.fab.setOnClickListener(View.OnClickListener { showBottomDialog() })
-    }
-    private fun replaceFragment(fragment: Fragment) {
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.fragment, fragment)
-        fragmentTransaction.commit()
+
+
+        binding.fab.setOnClickListener{
+            showBottomDialog() }
+
     }
     private fun showBottomDialog() {
         val dialog = Dialog(this)
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
         dialog.setContentView(R.layout.bottomsheetlayout)
         val cancelButton = dialog.findViewById<ImageView>(R.id.cancelButton)
 
@@ -61,7 +57,7 @@ open class MainActivity : AppCompatActivity() {
         dialog.show()
         dialog.window!!.setLayout(
             ViewGroup.LayoutParams.MATCH_PARENT,
-            ViewGroup.LayoutParams.WRAP_CONTENT
+            ViewGroup.LayoutParams.MATCH_PARENT
         )
         dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         dialog.window!!.attributes.windowAnimations = R.style.DialogAnimation
