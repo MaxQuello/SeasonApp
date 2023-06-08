8ecac1109369dfa9131f7d73abb23e6e194a76e5
gscaglione <gioachino.scaglione@community.unipa.it>
2023-06-08 18:28:06 +0200

    Versione 08/06 ore 18:34 porcodiddio
    problema tasto fluttuante che diocane apre il fragment prenota a metà la prima volta e dalla seconda in poi non lo carica più.

--- app/src/main/java/com/example/seasonapp/MainActivity.kt
+++ app/src/main/java/com/example/seasonapp/MainActivity.kt
@@ -1,100 +1,92 @@
 package com.example.seasonapp
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import android.view.Window
+import android.widget.ImageView
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.findFragment
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import com.example.seasonapp.databinding.ActivityMainBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.navigation.NavigationView
 
 open class MainActivity : AppCompatActivity() {
-    private lateinit var drawerLayout: DrawerLayout
     private lateinit var binding : ActivityMainBinding
+    private lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         drawerLayout = findViewById(R.id.drawer_layout)
-        val navigationView: NavigationView = findViewById(R.id.nav_view)
-
-        binding.fab.setOnClickListener {
-          /*  val fragment = binding.homeLayout.findFragment<Fragment>()
-            val transaction = supportFragmentManager.beginTransaction()
-            transaction.setCustomAnimations(R.anim.slide_in, R.anim.slide_out)
-            transaction.replace()*/
-        }
 
         val toolbar = binding.toolbar2
         setSupportActionBar(toolbar)
         toolbar.setNavigationOnClickListener() {
-                    drawerLayout.openDrawer(GravityCompat.START)
-                    true
-                }
-
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
-        val navController = findNavController(R.id.fragment)
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.homeFragment,
-                R.id.notifyFragment,
-                R.id.contattiFragment,
-                R.id.profileFragment
-            )
-        )
-        bottomNavigationView.setupWithNavController(navController)
-
-    }
-
-        /*
-            super.setSupportActionBar(toolbar)
-
-                val toggle = ActionBarDrawerToggle(
-                this, drawerLayout, /*toolbar,*/ R.string.drawer_open, R.string.drawer_close
-            )
-            drawerLayout.addDrawerListener(toggle)
-            toggle.syncState()
-
-            if (savedInstanceState == null) {
-                supportFragmentManager.beginTransaction()
-                    .replace(R.id.fragment, HomeFragment())
-                    .commit()
-                val menu = navigationView.menu
-                menu.findItem(R.id.homeFragment)?.isChecked = true
-            }
-        */
-        public fun onNavigationItemSelected(item: MenuItem): Boolean {
-            val fragment: Fragment = when (item.itemId) {
-                R.id.camereFragment -> CamereFragment()
-                R.id.ristoranteFragment -> RistoranteFragment()
-                R.id.serviziFragment -> ServiziFragment()
-                else -> throw IllegalArgumentException("Invalid menu item ID")
-            }
-
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment, fragment)
-                .commit()
-
-            drawerLayout.closeDrawer(GravityCompat.START)
-            return true
-    }
-
-            override fun onBackPressed() {
-            if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-                drawerLayout.closeDrawer(GravityCompat.START)
-            } else {
-                super.onBackPressed()
-            }
+            drawerLayout.openDrawer(GravityCompat.START)
+            true
         }
+
+        //   replaceFragment(HomeFragment())
+
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.homeFragment -> replaceFragment(HomeFragment())
+                R.id.notifyFragment -> replaceFragment(NotifyFragment())
+                R.id.contattiFragment -> replaceFragment(NotifyFragment())
+                R.id.profileFragment -> replaceFragment(ProfileFragment())
+            }
+            true
+        }
+        binding.fab.setOnClickListener(View.OnClickListener { showBottomDialog() })
     }
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment, fragment)
+        fragmentTransaction.commit()
+    }
+    private fun showBottomDialog() {
+        val dialog = Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.bottomsheetlayout)
+        val cancelButton = dialog.findViewById<ImageView>(R.id.cancelButton)
+
+        cancelButton.setOnClickListener { dialog.dismiss() }
+        dialog.show()
+        dialog.window!!.setLayout(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+        dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.window!!.attributes.windowAnimations = R.style.DialogAnimation
+        dialog.window!!.setGravity(Gravity.TOP)
+    }
+
+    fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val fragment: Fragment = when (item.itemId) {
+            R.id.camereFragment -> CamereFragment()
+            R.id.ristoranteFragment -> RistoranteFragment()
+            R.id.serviziFragment -> ServiziFragment()
+            else -> throw IllegalArgumentException("Invalid menu item ID")
+        }
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment, fragment)
+            .commit()
+
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+}
+
+
 
 
 
