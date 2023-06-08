d7b4ef02c6fcda184f61a0d6cc41631a5c88325c
gscaglione <gioachino.scaglione@community.unipa.it>
2023-06-08 15:29:18 +0200

    Versione 08/06 ore 15:29 porcodiddio2

--- app/src/main/java/com/example/seasonapp/MainActivity.kt
+++ app/src/main/java/com/example/seasonapp/MainActivity.kt
@@ -3,31 +3,41 @@ package com.example.seasonapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
-import android.widget.Toolbar
-import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import androidx.fragment.app.findFragment
+import com.example.seasonapp.databinding.ActivityMainBinding
 import com.google.android.material.navigation.NavigationView
 
 open class MainActivity : AppCompatActivity() {
     private lateinit var drawerLayout: DrawerLayout
-    private lateinit var navigationView: NavigationView
-
-
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        supportActionBar?.hide()
-/*
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         drawerLayout = findViewById(R.id.drawer_layout)
         val navigationView: NavigationView = findViewById(R.id.nav_view)
 
-        //var toolbar: Toolbar = findViewById(R.id.toolbar)
-        //fun setSupportActionBar(toolbar: androidx.appcompat.widget.Toolbar?) {
-        //    super.setSupportActionBar(toolbar)
+        binding.fab.setOnClickListener {
+          /*  val fragment = binding.homeLayout.findFragment<Fragment>()
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.setCustomAnimations(R.anim.slide_in, R.anim.slide_out)
+            transaction.replace()*/
+        }
+
+        val toolbar = binding.toolbar2
+        setSupportActionBar(toolbar)
+        toolbar.setNavigationOnClickListener() {
+                    drawerLayout.openDrawer(GravityCompat.START)
+                    true
+                }
+        }
+
+        /*{
+            super.setSupportActionBar(toolbar)
 
                 val toggle = ActionBarDrawerToggle(
                 this, drawerLayout, /*toolbar,*/ R.string.drawer_open, R.string.drawer_close
@@ -35,16 +45,15 @@ open class MainActivity : AppCompatActivity() {
             drawerLayout.addDrawerListener(toggle)
             toggle.syncState()
 
-            /if (savedInstanceState == null) {
+            if (savedInstanceState == null) {
                 supportFragmentManager.beginTransaction()
                     .replace(R.id.fragment, HomeFragment())
                     .commit()
                 val menu = navigationView.menu
                 menu.findItem(R.id.homeFragment)?.isChecked = true
             }
-        }
-
-            fun onNavigationItemSelected(item: MenuItem): Boolean {
+        }*/
+        fun onNavigationItemSelected(item: MenuItem): Boolean {
             val fragment: Fragment = when (item.itemId) {
                 R.id.camereFragment -> CamereFragment()
                 R.id.ristoranteFragment -> RistoranteFragment()
@@ -58,14 +67,14 @@ open class MainActivity : AppCompatActivity() {
 
             drawerLayout.closeDrawer(GravityCompat.START)
             return true
-        }
+    }
 
             override fun onBackPressed() {
             if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
                 drawerLayout.closeDrawer(GravityCompat.START)
             } else {
                 super.onBackPressed()
-            }                                                             */
+            }
         }
     }
 
