bf99dd62f6232d770fa551ccaa54e6cd4e75a579
gscaglione <gioachino.scaglione@community.unipa.it>
2023-07-01 21:11:43 +0200

    FUNZIONA IL PROFILO CON I TASTI CHE SCOMPAIONO SE SEI LOGGATO

--- app/src/main/java/com/example/seasonapp/ProfileFragment.kt
+++ app/src/main/java/com/example/seasonapp/ProfileFragment.kt
@@ -1,6 +1,8 @@
 package com.example.seasonapp
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -26,7 +28,7 @@ class ProfileFragment : Fragment() {
         val view = binding.root
         binding.buttonAccedi
         binding.buttonRegistrati
-
+        checkiflogindone()
         val nomeUtente = "user"  //da settare con il db
         val messaggioBenvenuto = getString(R.string.benvenuto, nomeUtente)
         binding.testoProfilo.text = messaggioBenvenuto
@@ -57,7 +59,30 @@ class ProfileFragment : Fragment() {
             findNavController().navigate(R.id.action_profileFragment_to_gestisciServiziFragment)
 
         }
+
         return view
+    }
+
+    private fun checkiflogindone(): Boolean {
+        // Ottenere un'istanza delle SharedPreferences
+        val sharedPreferences = requireActivity().getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+
+        // Recuperare lo stato del login
+        val isLoggedIn = sharedPreferences.getBoolean("isLoggedIn", false)
+
+        // Verificare lo stato del login
+        if (isLoggedIn) {
+            binding.conditionLayout.visibility = View.GONE
+            binding.buttonLayout.visibility = View.VISIBLE
+            Log.i("PROVA","LOGIN FATTO")
+            return true
+        } else {
+            binding.conditionLayout.visibility = View.VISIBLE
+            binding.buttonLayout.visibility = View.GONE
+            Log.i("PROVA","LOGIN NON FATTO")
+            return false
+        }
+
     }
 
 }
