3e808136fad2a682a944355e3f500ca4dfb258bb
Maxquello <massimobonafede321@gmail.com>
2023-07-02 12:27:16 +0200

    V1.12.1 : Verificato che otp uguale a quello inserito

--- app/src/main/java/com/example/seasonapp/profilazione/OtpFragment.kt
+++ app/src/main/java/com/example/seasonapp/profilazione/OtpFragment.kt
@@ -1,36 +1,107 @@
 package com.example.seasonapp.profilazione
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.seasonapp.R
+import com.example.seasonapp.api.ClientNetwork
+import com.example.seasonapp.databinding.FragmentOtpBinding
+import com.example.seasonapp.databinding.FragmentPrenotaBinding
+import com.google.gson.JsonObject
+import com.google.gson.JsonParser
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class OtpFragment : Fragment() {
+    private lateinit var binding : FragmentOtpBinding
+    private lateinit var buttonContinua : Button
+    private lateinit var buttonIndietro : Button
+    private lateinit var editTextOtp : EditText
+    private var editTextOtpString : Int? = null
+    private var codiceOtp: Int = 0
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
 
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_otp, container, false)
-        val buttonIndietro = view.findViewById<ImageButton>(R.id.buttonIndietro3)
-        buttonIndietro.setOnClickListener {
+
+        binding = FragmentOtpBinding.inflate(inflater)
+
+
+        val buttonIndietro = view?.findViewById<ImageButton>(R.id.buttonIndietro3)
+        buttonIndietro?.setOnClickListener {
             findNavController().navigate(R.id.action_otpFragment_to_recuperoPasswordFragment)
 
         }
-        //QUERY DB PER CONTROLLARE ESATTEZZA DELL'OTP INSERITO
-        val buttonContinua = view.findViewById<Button>(R.id.buttonContinua)
+
+
+        buttonContinua = binding.buttonContinua
         buttonContinua.setOnClickListener {
-            findNavController().navigate(R.id.action_otpFragment_to_modificaPasswordFragment)
+            editTextOtp = binding.otp
+            editTextOtpString= editTextOtp.text.toString().toInt()
+            verificaOtp()
+
 
         }
-        return view
+        return binding.root
+    }
+
+    private fun verificaOtp() {
+        val query = "SELECT codice_otp from otp WHERE codice_otp = ${editTextOtpString}"
+        ClientNetwork.retrofit.otp(query).enqueue(
+            object : Callback<JsonObject>{
+                override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
+                    if (response.isSuccessful) {
+                        val responseBody = response.body()
+                        val codiceOtpJsonArray = responseBody?.getAsJsonArray("queryset")
+
+                        if (codiceOtpJsonArray != null && codiceOtpJsonArray.size() > 0) {
+                            val codiceOtpJson = codiceOtpJsonArray[0].asJsonObject
+                                .get("codice_otp")
+
+                            codiceOtp = codiceOtpJson?.asInt ?: 0
+
+                            if (codiceOtp == editTextOtpString) {
+                                findNavController().navigate(R.id.action_otpFragment_to_modificaPasswordFragment)
+                            } else {
+                                Toast.makeText(
+                                    requireContext(),
+                                    "Codice otp sbagliato, riprovare",
+                                    Toast.LENGTH_LONG
+                                ).show()
+                            }
+                        } else {
+                            // L'array "queryset" è vuoto, gestisci il caso appropriato
+                            Toast.makeText(
+                                requireContext(),
+                                "Codice otp sbagliato, riprovare",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
+                    } else {
+                        Log.d("PROBLEMA","PROBLEMA")
+                    }
+
+                }
+
+                override fun onFailure(call: Call<JsonObject>, t: Throwable) {
+                    Log.i("LOG-Login_Fragment-onFailure", "Errore accesso ${t.message}")
+                    Toast.makeText(context, t.message, Toast.LENGTH_SHORT).show()
+                }
+
+            }
+        )
     }
 
 }
