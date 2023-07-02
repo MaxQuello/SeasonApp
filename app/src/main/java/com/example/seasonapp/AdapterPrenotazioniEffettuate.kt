3e08bb293a4f5b813ca4d5c6a5a5c74535ac81f5
gscaglione <gioachino.scaglione@community.unipa.it>
2023-07-02 19:35:51 +0200

    adapter

--- app/src/main/java/com/example/seasonapp/AdapterPrenotazioniEffettuate.kt
+++ app/src/main/java/com/example/seasonapp/AdapterPrenotazioniEffettuate.kt
@@ -1,21 +1,33 @@
 package com.example.seasonapp
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.seasonapp.databinding.CardViewPrenotazioniEffettuateBinding
+import java.time.LocalDate
 
 class AdapterPrenotazioniEffettuate(private val data: ArrayList<PrenotazioniEffettuate>): RecyclerView.Adapter<AdapterPrenotazioniEffettuate.ViewHolder>() {
     class ViewHolder(val binding: CardViewPrenotazioniEffettuateBinding): RecyclerView.ViewHolder(binding.root) {
-        fun bind(item: PrenotazioniEffettuate){
+        fun bind(item: PrenotazioniEffettuate) {
             binding.idPrenotazione.text = item.idPrenotazione.toString()
             binding.idUtentePren.text = item.idUtente.toString()
             binding.dataCheckIn2.text = item.dataCheckIn.toString()
             binding.dataCheckOut2.text = item.dataCheckOut.toString()
+            val currentDate: LocalDate = LocalDate.now()
+            if (item.dataCheckIn!! < currentDate) {
+                binding.buttonCancellaPrenotazione.visibility = View.GONE
+            } else {
+                binding.buttonCancellaPrenotazione.visibility = View.VISIBLE
+            }
+            binding.buttonCancellaPrenotazione.setOnClickListener {
+
+
+            }
+
         }
+
     }
-
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = CardViewPrenotazioniEffettuateBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(view)
