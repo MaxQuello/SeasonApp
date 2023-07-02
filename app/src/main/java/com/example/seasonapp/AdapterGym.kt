d2d761dc702357be38160b8f0ac2239a4dd2603f
gscaglione <gioachino.scaglione@community.unipa.it>
2023-07-02 18:15:48 +0200

    servizi

--- app/src/main/java/com/example/seasonapp/AdapterGym.kt
+++ app/src/main/java/com/example/seasonapp/AdapterGym.kt
@@ -1,15 +1,17 @@
+package com.example.seasonapp
+
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.seasonapp.Gym
-import com.example.seasonapp.Recensioni
 import com.example.seasonapp.databinding.CardViewGympassBinding
 
 
 class AdapterGym(private val data: ArrayList<Gym>): RecyclerView.Adapter<AdapterGym.ViewHolder>() {
     class ViewHolder(val binding: CardViewGympassBinding): RecyclerView.ViewHolder(binding.root) {
         fun bind(item: Gym){
-
+            binding.idPrenotazioneGym.text = item.id_prenotazione.toString()
+            binding.nOspitiGym.text = item.nOspiti.toString()
+            binding.dataPrenotazioneGym.text = item.data_prenotazione.toString()
         }
     }
 
--- app/src/main/java/com/example/seasonapp/GestisciServiziFragment.kt
+++ app/src/main/java/com/example/seasonapp/GestisciServiziFragment.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.seasonapp.api.ClientNetwork
 import com.example.seasonapp.data.DbManager
 import com.example.seasonapp.data.SessionManager
@@ -16,11 +17,16 @@ import com.google.gson.JsonObject
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class GestisciServiziFragment : Fragment() {
     private lateinit var binding : FragmentGestisciServiziBinding
     private var idUtente : Int? = null
     private lateinit var dbManager: DbManager
+    private lateinit var palestra : ArrayList<Gym>
+    private lateinit var ristorante : ArrayList<Ristorante>
+    private lateinit var impianto : ArrayList<Impianto>
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -40,6 +46,8 @@ class GestisciServiziFragment : Fragment() {
 
         if(checkiflogindone()){
             trovaprenotazionipalestra()
+            trovaprenotazioniImpianti()
+            trovaprenotazioniRistorante()
         }
 
 
@@ -52,16 +60,34 @@ class GestisciServiziFragment : Fragment() {
         ClientNetwork.retrofit.getMyGymReservation(query).enqueue(
             object : Callback<JsonObject>{
                 override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
+                    val bodyString = response.body()
                     if (response.isSuccessful){
-                        Log.d("PROVA","${response.body()}")
-                    }else{
-                        Toast.makeText(
-                            requireContext(),
-                            "Andata male",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-                }
+                        palestra = ArrayList<Gym>()
+                        val jsonArray = response.body()?.getAsJsonArray("queryset")
+                        Log.d("QUERY","risultati:  ${jsonArray}")
+                        val resultList = jsonArray?.mapNotNull { it as? JsonObject }
+                        Log.d("listarisultati","lista risultati:  ${resultList}")
+                        if (resultList != null) {
+                            for (jsonObject in resultList) {
+                                val id_prenotazione = jsonObject["id"].toString().toInt()
+                                val nOspiti = jsonObject["numero_ospiti"].toString().toInt()
+                                val dataPrenotazione =
+                                    jsonObject["data_prenotazione"].toString().replace("\"", "")
+                                val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+                                val dateRes = LocalDate.parse(dataPrenotazione, formatter)
+                                palestra.add(Gym(id_prenotazione, dateRes, nOspiti))
+                            }
+                        }
+                        binding.listaGym.adapter = AdapterGym(palestra)
+                        binding.listaGym.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+                            }else{
+                                Toast.makeText(
+                                    requireContext(),
+                                    "Andata male",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        }
 
                 override fun onFailure(call: Call<JsonObject>, t: Throwable) {
                     Log.i("LOG-Gestisci_Servizi-onFailure", "Errore accesso ${t.message}")
@@ -73,17 +99,34 @@ class GestisciServiziFragment : Fragment() {
     }
 
     private fun trovaprenotazioniImpianti() {
-        val query = "SELECT * FROM prenotazioneImpianti WHERE id_utente = ${idUtente}"
+        val query = "SELECT * FROM prenotazioneImpianti WHERE ref_impianti = ${idUtente}"
 
-        ClientNetwork.retrofit.getMyGymReservation(query).enqueue(
+        ClientNetwork.retrofit.getMyImpiantiReservation(query).enqueue(
             object : Callback<JsonObject>{
                 override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
+                    val bodyString = response.body()
                     if (response.isSuccessful){
-                        Log.d("PROVA","${response.body()}")
+                        impianto = ArrayList<Impianto>()
+                        val jsonArray = response.body()?.getAsJsonArray("queryset")
+                        val resultList = jsonArray?.mapNotNull { it as? JsonObject }
+                        Log.d("listarisultati","lista risultati:  ${resultList}")
+                        if (resultList != null) {
+                            for (jsonObject in resultList) {
+                                val id_prenotazione = jsonObject["id_prenotazione_impianto"].toString().toInt()
+                                val id_utente = jsonObject["ref_impianti"].toString().toInt()
+                                val dataPrenotazione =
+                                    jsonObject["data_prenotazione_impianti"].toString().replace("\"", "")
+                                val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+                                val dateRes = LocalDate.parse(dataPrenotazione, formatter)
+                                impianto.add(Impianto(id_prenotazione, dateRes, id_utente))
+                            }
+                        }
+                        binding.listaImpianto.adapter = AdapterImpianto(impianto)
+                        binding.listaImpianto.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
                     }else{
                         Toast.makeText(
                             requireContext(),
-                            "Andata male",
+                            "Query Impianti Error",
                             Toast.LENGTH_SHORT
                         ).show()
                     }
@@ -104,8 +147,27 @@ class GestisciServiziFragment : Fragment() {
         ClientNetwork.retrofit.getMyGymReservation(query).enqueue(
             object : Callback<JsonObject>{
                 override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
+                    val bodyString = response.body()
                     if (response.isSuccessful){
-                        Log.d("PROVA","${response.body()}")
+                        ristorante = ArrayList<Ristorante>()
+                        val jsonArray = response.body()?.getAsJsonArray("queryset")
+                        Log.d("QUERY","risultati:  ${jsonArray}")
+                        val resultList = jsonArray?.mapNotNull { it as? JsonObject }
+                        Log.d("listarisultati","lista risultati:  ${resultList}")
+                        if (resultList != null) {
+                            for (jsonObject in resultList) {
+                                val id_prenotazione = jsonObject["id"].toString().toInt()
+                                val nOspiti = jsonObject["numero_ospiti"].toString().toInt()
+                                val orario = jsonObject["chosen_meal"].toString()
+                                val dataPrenotazione =
+                                    jsonObject["data_prenotazione"].toString().replace("\"", "")
+                                val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+                                val dateRes = LocalDate.parse(dataPrenotazione, formatter)
+                                ristorante.add(Ristorante(id_prenotazione, dateRes, nOspiti, orario))
+                            }
+                        }
+                        binding.listaRistorante.adapter = AdapterRistorante(ristorante)
+                        binding.listaRistorante.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
                     }else{
                         Toast.makeText(
                             requireContext(),
